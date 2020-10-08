d8580dee7fbec6c49df197b3fc7e4bb80179d0ae
Kabir Khan <kkhan@redhat.com>
2020-10-08 09:56:53 +0100

    Explicitly mark deployments with CDI as CDI dependencies

--- subsystems/reactive-messaging/src/main/java/org/wildfly/extension/microprofile/reactive/messaging/deployment/ReactiveMessagingDependencyProcessor.java
+++ subsystems/reactive-messaging/src/main/java/org/wildfly/extension/microprofile/reactive/messaging/deployment/ReactiveMessagingDependencyProcessor.java
@@ -66,7 +66,9 @@ public class ReactiveMessagingDependencyProcessor implements DeploymentUnitProce
         final ModuleLoader moduleLoader = Module.getBootModuleLoader();
 
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, "org.eclipse.microprofile.reactive-messaging.api", false, false, true, false));
-        moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, "io.smallrye.reactive.messaging", false, false, true, false));
+        moduleSpecification.addSystemDependency(
+                cdiDependency(
+                        new ModuleDependency(moduleLoader, "io.smallrye.reactive.messaging", false, false, true, false)));
 
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, "io.smallrye.config", false, false, true, false));
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, "org.eclipse.microprofile.config.api", false, false, true, false));
@@ -111,5 +113,12 @@ public class ReactiveMessagingDependencyProcessor implements DeploymentUnitProce
         }
         MicroProfileReactiveMessagingLogger.LOGGER.debugf("Deployment '%s' is not a MicroProfile Fault Tolerance deployment.", deploymentUnit.getName());
         return false;
+    }
+
+
+    private ModuleDependency cdiDependency(ModuleDependency moduleDependency) {
+        // This is needed following https://issues.redhat.com/browse/WFLY-13641 / https://github.com/wildfly/wildfly/pull/13406
+        moduleDependency.addImportFilter(s -> s.equals("META-INF"), true);
+        return moduleDependency;
     }
 }

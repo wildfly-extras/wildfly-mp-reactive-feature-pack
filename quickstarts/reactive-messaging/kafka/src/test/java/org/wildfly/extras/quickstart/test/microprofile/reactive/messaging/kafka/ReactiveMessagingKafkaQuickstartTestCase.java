94cff81385ad85a9f5db27b3c0be2b09ad1b5a36
Kabir Khan <kkhan@redhat.com>
2021-02-23 12:52:07 +0000

    Move to StrimziKafkaContainer for use with testcontainers

--- quickstarts/reactive-messaging/kafka/src/test/java/org/wildfly/extras/quickstart/test/microprofile/reactive/messaging/kafka/ReactiveMessagingKafkaQuickstartTestCase.java
+++ quickstarts/reactive-messaging/kafka/src/test/java/org/wildfly/extras/quickstart/test/microprofile/reactive/messaging/kafka/ReactiveMessagingKafkaQuickstartTestCase.java
@@ -16,6 +16,8 @@
 
 package org.wildfly.extras.quickstart.test.microprofile.reactive.messaging.kafka;
 
+import static org.awaitility.Awaitility.await;
+
 import java.io.File;
 import java.net.URL;
 import java.util.List;
@@ -41,7 +43,9 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.testcontainers.containers.KafkaContainer;
+
+import io.strimzi.StrimziKafkaContainer;
+
 /**
  * @author <a href="mailto:kabir.khan@jboss.com">Kabir Khan</a>
  */
@@ -50,7 +54,6 @@ import org.testcontainers.containers.KafkaContainer;
 @ServerSetup({ReactiveMessagingKafkaQuickstartTestCase.KafkaBootstrapServerSetupTask.class,
         ReactiveMessagingKafkaQuickstartTestCase.AllowExperimentalAnnotationsSetupTask.class})
 public class ReactiveMessagingKafkaQuickstartTestCase {
-    private static final KafkaContainer KAFKA = new KafkaContainer();
 
     @ArquillianResource
     URL url;
@@ -100,11 +103,12 @@ public class ReactiveMessagingKafkaQuickstartTestCase {
 
     public static class KafkaBootstrapServerSetupTask extends CLIServerSetupTask {
 
+        private StrimziKafkaContainer kafka;
         @Override
         public void setup(ManagementClient managementClient, String containerId) throws Exception {
-            KAFKA.start();
+            startKafkaBroker();
             this.builder.node("default")
-                    .setup("/system-property=kafka.bootstrap.servers:add(value=" + KAFKA.getBootstrapServers() + ")")
+                    .setup("/system-property=kafka.bootstrap.servers:add(value=" + kafka.getBootstrapServers() + ")")
                     .teardown("/system-property=kafka.bootstrap.servers:remove");
             super.setup(managementClient, containerId);
         }
@@ -114,7 +118,25 @@ public class ReactiveMessagingKafkaQuickstartTestCase {
             try {
                 super.tearDown(managementClient, containerId);
             } finally {
-                KAFKA.stop();
+                stopKafkaBroker();
+            }
+        }
+
+        public void startKafkaBroker() {
+
+            kafka = new StrimziKafkaContainer();
+            kafka.start();
+            await().until(() -> kafka.isRunning());
+        }
+
+        public void stopKafkaBroker() {
+            if (kafka != null) {
+                try {
+                    kafka.stop();
+                } catch (Exception e) {
+                    // Ignore it.
+                }
+                await().until(() -> !kafka.isRunning());
             }
         }
     }
